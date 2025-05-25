6051612c7d0919cc6e82831ea3f7057093502268
Tarun Pable <professional.tarunpable@gmail.com>
2025-05-25 20:36:52 +0530

    Minor changes

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,10 @@ npm run build
 cd dist
 
 git init
-# git remote add origin https://github.com/troll-warlord/alphabets.git
 git config user.name "${GIT_USER_NAME}"
 git config user.email "${GIT_USER_EMAIL}"
 
 git add -A
 git commit -m "Deploying application"
 
-# git push -f origin master:live
 git push -f https://x-access-token:${GH_TOKEN}@github.com/troll-warlord/alphabets.git master:live
