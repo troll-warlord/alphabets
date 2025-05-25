dd7943c27a9a50f5cd49cf818ab87c4cd3419617
Tarun Pable <professional.tarunpable@gmail.com>
2025-05-25 12:38:24 +0530

    Minor changes

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,9 @@ npm run build
 cd dist
 
 git init
-git add -A
-git commit -m "Deploying application"
 git remote add origin https://github.com/troll-warlord/alphabets.git
 
-git push --force-with-lease origin master:live
+git add -A
+git commit -m "Deploying application"
+
+git push -f origin master:live
